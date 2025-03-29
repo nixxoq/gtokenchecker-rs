ae2ef1d24034774ee514a12def9f9077c09411b6
nixxoq <nixxoq@outlook.com>
2025-03-29 17:40:48 +0200

    idk how to summarize it so check description
    chore(crate): reorganize Utils module to `utils.rs` (main crate), `utils/constants.rs` (constants), `utils/enums.rs` (enums), `utils/structs.rs` (structs)
    
    [utils.enums]: implemented `std::error:Error`, `reqwest::Error`, `serde_json::Error` trait for `ApiError` enum
    
    [core.api]: implement lifetime for the `API` structure , use &self (borrowed self) instead of standard self (i don't use all class functions, so i don't want clone this structure every time)
    
    [core.utils]: using `gen_url` func in `get_banner`

--- src/api.rs
+++ src/api.rs
@@ -1,126 +1,25 @@
-use reqwest::{self, StatusCode};
-use serde::{Deserialize, Serialize};
+use reqwest::{
+    self, StatusCode,
+    header::{self, HeaderMap, HeaderValue},
+};
 use serde_json::Value;
 
-use crate::utils::{BannerType, USER_FLAGS, Utils};
+use crate::utils::{
+    Utils,
+    constants::USER_FLAGS,
+    enums::{ApiError, BannerType},
+    structs::{Connection, Promotion, TokenInfo, TokenResult, UnauthorizedResponse},
+};
 
-#[derive(Serialize, Deserialize, Debug)]
-pub struct UnauthorizedResponse {
-    pub code: i32,
-    pub message: String,
-}
-
-#[derive(Serialize, Deserialize, Debug)]
-pub struct Connection {
-    #[serde(rename = "type")]
-    pub connection_type: String,
-    pub name: String,
-    pub visibility: u8,
-    pub verified: bool,
-    pub revoked: bool,
-}
-
-#[derive(Serialize, Deserialize, Debug)]
-pub struct Promotion {
-    outbound_title: String,
-    start_time: String,
-    end_date: String,
-    #[serde(rename = "outbound_redemption_page_link")]
-    link: String,
-    code: String,
-}
-
-pub struct TokenResult {
-    pub main_info: TokenInfo,
-    pub connections: Vec<Connection>,
-    pub promotions: Vec<Promotion>,
-}
-
-impl TokenResult {
-    pub fn show(self, mask_token: bool) {
-        self.main_info.show(mask_token);
-        self.connections
-            .iter()
-            .for_each(|connection| println!("{}: {}", connection.connection_type, connection.name));
-    }
-}
-
-#[derive(Serialize, Deserialize, Debug, Clone)]
-pub struct TokenInfo {
-    pub id: String,
-    pub username: String,
-    pub global_name: String,
-    pub discriminator: String,
-    pub avatar: Option<String>,
-    pub banner: Option<String>,
-    pub banner_color: String,
-    pub email: String,
-    pub locale: String,
-    // pub pronouns: Option<String>,
-    pub phone: Option<String>,
-    pub mfa_enabled: bool,
-    pub bio: Option<String>,
-    pub public_flags: i128,
-
-    // Skip these fields because discord api cannot return them and we will add these fields later during initialization process.
-    #[serde(skip)]
-    pub fullname: String,
-    #[serde(skip)]
-    pub token: String,
-}
-
-impl TokenInfo {
-    pub fn show(self, mask_token: bool) {
-        let token = if mask_token {
-            let mut parts: Vec<_> = self.token.split('.').map(|part| part.to_string()).collect();
-            parts.last_mut().map(|last| *last = "*".repeat(last.len()));
-            parts.join(".")
-        } else {
-            self.token.clone()
-        };
-
-        println!(
-            "
-Token: {}
-
-ID: {}
-username: {}
-Full name: {}
-Avatar: {}
-Banner: {}
-Banner color: {}
-E-mail: {}
-Phone: {}
-MFA: {}
-Bio: {}",
-            // pronouns: {}
-            token,
-            self.id,
-            self.username,
-            self.fullname,
-            self.avatar.unwrap_or(String::from("No banner provided")),
-            self.banner.unwrap_or(String::from("No banner provided")),
-            self.banner_color,
-            self.email,
-            self.phone.unwrap_or(String::from("No phone provided")),
-            self.mfa_enabled,
-            self.bio.unwrap_or(String::from("No bio provided")),
-            // self.pronouns
-            //     .unwrap_or(String::from("No pronouns available"))
-        )
-    }
-}
-
-#[derive(Clone)]
 pub struct API<'a> {
     token: String,
     client: &'a reqwest::Client,
 }
 
-impl API<'_> {
+impl<'a> API<'a> {
     pub const API_URL: &'static str = "https://discord.com/api/v9";
 
-    pub fn new(token: String, client: &reqwest::Client) -> API {
+    pub fn new(token: String, client: &'a reqwest::Client) -> API<'a> {
         API {
             token: token,
             client: client,
@@ -129,62 +28,65 @@ impl API<'_> {
 
     // TODO: instead of initializing reqwest client every time, require in the get_me, and other functions in future, client parameter (&reqwest::Client)
     // UPD: don't forget to remove this once all function from original GTokenChecker will be migrated
-    async fn get_me(
-        self,
-        // client: &reqwest::Client,
-        // token: &str,
-    ) -> Result<TokenInfo, UnauthorizedResponse> {
+    async fn get_me(&self) -> Result<TokenInfo, ApiError> {
         let response = self
             .client
             .get(format!("{}/users/@me", API::API_URL))
-            .header("Authorization", &self.token)
             .send()
-            .await
-            .unwrap();
+            .await?;
 
-        match response.status() {
+        let status = response.status();
+        match status {
             StatusCode::OK => {
-                let text = response.text().await.unwrap();
-                let raw_json: Value = serde_json::from_str(&text).unwrap();
+                let text = response.text().await?;
+                let raw_json: Value = serde_json::from_str(&text)?;
 
                 // HINT Remove this line after researching results
                 dbg!("{}", raw_json); // let it show some info for adding new features
 
-                let mut token_info: TokenInfo = serde_json::from_str(&text).unwrap();
+                let mut token_info: TokenInfo = serde_json::from_str(&text)?;
                 token_info.fullname =
                     format!("{}#{}", token_info.username, token_info.discriminator);
-                token_info.token = self.token;
+                token_info.token = self.token.clone();
 
-                token_info.avatar = match token_info.avatar {
-                    Some(hash) => Some(Utils::get_avatar(&token_info.id, &hash)),
-                    None => Some(String::from("No avatar available")),
-                };
-                token_info.banner = match token_info.banner {
-                    Some(hash) => Some(Utils::get_banner(BannerType::User, &token_info.id, &hash)),
-                    None => Some(String::from("No banner provided")),
-                };
+                token_info.avatar = token_info
+                    .avatar
+                    .map(|hash| Utils::get_avatar(&token_info.id, &hash))
+                    .or_else(|| Some(String::from("No avatar available")));
+
+                token_info.banner = token_info
+                    .banner
+                    .map(|hash| Utils::get_banner(BannerType::User, &token_info.id, &hash))
+                    .or_else(|| Some(String::from("No banner available")));
 
                 Ok(token_info)
             }
             StatusCode::UNAUTHORIZED => {
-                let unauthorized_response: UnauthorizedResponse = response.json().await.unwrap();
-                Err(unauthorized_response)
+                let unauthorized_response: UnauthorizedResponse = response
+                    .json()
+                    .await
+                    .unwrap_or_else(|_| UnauthorizedResponse {
+                        code: status.as_u16() as i32,
+                        message: "Unauthorized (failed to parse error body)".to_string(),
+                    });
+                Err(ApiError::Unauthorized(unauthorized_response))
             }
             _ => {
-                let unauthorized_response = UnauthorizedResponse {
-                    code: response.status().as_u16() as i32,
-                    message: format!("Unexpected status code: {}", response.status()),
-                };
-                Err(unauthorized_response)
+                let error_body = response.text().await.unwrap_or_else(|_| {
+                    status
+                        .canonical_reason()
+                        .unwrap_or("Unknown error")
+                        .to_string()
+                });
+                Err(ApiError::UnexpectedStatus(status, error_body))
             }
         }
     }
 
-    pub async fn get_connections(self) -> Result<Vec<Connection>, UnauthorizedResponse> {
+    pub async fn get_connections(&self) -> Result<Vec<Connection>, UnauthorizedResponse> {
         let response = self
             .client
             .get(format!("{}/users/@me/connections", API::API_URL))
-            .header("Authorization", &self.token)
             .send()
             .await
             .unwrap();
@@ -209,17 +111,16 @@ impl API<'_> {
     }
 
     pub async fn get_promotions(
-        self,
-        locale: Option<String>,
+        &self,
+        locale: Option<&str>,
     ) -> Result<Vec<Promotion>, UnauthorizedResponse> {
         let response = self
             .client
             .get(format!(
                 "{}/users/@me/outbound-promotions/codes?locale={}",
                 API::API_URL,
-                locale.unwrap_or(String::from("us"))
+                locale.unwrap_or("us")
             ))
-            .header("Authorization", &self.token)
             .send()
             .await
             .unwrap();
@@ -251,35 +152,39 @@ pub struct Checker {
 
 impl Checker {
     pub fn new(token: &str) -> Self {
-        let client = reqwest::Client::builder().build().unwrap();
+        let mut headers = HeaderMap::new();
+        headers.insert(header::AUTHORIZATION, HeaderValue::from_str(token).unwrap());
+
+        let client = reqwest::Client::builder()
+            .default_headers(headers)
+            .build()
+            .unwrap();
         Checker {
-            client: client,
-            token: String::from(token),
+            client: client.to_owned(),
+            token: token.to_owned(),
         }
     }
 
-    async fn process_token(self, token: TokenInfo, api: API<'_>) -> TokenResult {
-        let connections = api.clone().get_connections().await.unwrap();
+    async fn process_token(&self, token: TokenInfo, api: &API<'_>) -> TokenResult {
+        let connections = api.get_connections().await.unwrap();
         let promotions = api
-            .clone()
-            .get_promotions(Some(token.clone().locale))
+            .get_promotions(Some(token.locale.as_ref()))
             .await
             .unwrap();
 
         TokenResult {
             main_info: token,
-            connections: connections,
-            promotions: promotions,
+            connections,
+            promotions,
         }
     }
 
-    pub async fn check(self) -> Result<TokenResult, UnauthorizedResponse> {
-        let client = self.client.clone();
-        let api = API::new(self.token.clone(), &client);
-        let token_result = api.clone().get_me().await;
+    pub async fn check(self) -> Result<TokenResult, ApiError> {
+        let api = API::new(self.token.clone(), &self.client);
+        let token_result = api.get_me().await;
 
         match token_result {
-            Ok(token) => Ok(self.process_token(token, api.clone()).await),
+            Ok(token) => Ok(self.process_token(token, &api).await),
             Err(resp) => return Err(resp),
         }
     }
--- src/utils.rs
+++ src/utils.rs
@@ -1,62 +1,12 @@
 use std::collections::HashMap;
 
 use chrono::DateTime;
+use constants::DISCORD_CDN_BASE;
+use enums::{BannerType, CdnType, ImageType, StrOrInt};
 
-pub enum StrOrInt {
-    StrV(String),
-    I32V(i32),
-    I64V(i64),
-    I128V(i128),
-}
-pub enum CdnType {
-    UserAvatar,
-    GuildIcon,
-    Banner(BannerType),
-}
-
-pub enum BannerType {
-    User,
-    Guild,
-}
-
-pub enum ImageType {
-    Png,
-    Jpg,
-    Jpeg,
-    Webp,
-    Gif,
-    Svg,
-}
-
-const DISCORD_CDN_BASE: &str = "https://cdn.discordapp.com";
-pub const USER_FLAGS: &[(i128, &str)] = &[
-    (1 << 0, "Staff"),
-    (1 << 1, "Guild Partner"),
-    (1 << 2, "HypeSquad Events Member"),
-    (1 << 3, "Bug Huner Level 1"),
-    (1 << 4, "SMS 2FA Enabled"),
-    (1 << 5, "Dismissed Nitro promotion"),
-    (1 << 6, "House Bravery Member"),
-    (1 << 7, "House Brilliance Member"),
-    (1 << 8, "House Balance Member"),
-    (1 << 9, "Early Nitro Supporter"),
-    (1 << 10, "Team Supporter"),
-    (1 << 13, "Unread urgent system messages"),
-    (1 << 14, "Bug Hunter Level 2"),
-    (1 << 15, "Under age account"),
-    (1 << 16, "Verified Bot"),
-    (1 << 17, "Early Verified Bot Developer"),
-    (1 << 18, "Moderator Programs Alumni"),
-    (1 << 19, "Bot uses only http interactions"),
-    (1 << 20, "Marked as spammer"),
-    (1 << 22, "Active Developer"),
-    (1 << 23, "Provisional Account"),
-    (1 << 33, "Global ratelimit"), // User has their global ratelimit raised to 1,200 requests per second
-    (1 << 34, "Deleted account"),
-    (1 << 35, "Disabled for suspicious activity"),
-    (1 << 36, "Self-deleted account"),
-    (1 << 41, "User account is disabled"),
-];
+pub mod constants;
+pub mod enums;
+pub mod structs;
 
 fn gen_url(cdn_type: CdnType, type_id: &String, hash: &String, image_type: ImageType) -> String {
     let result = format!(
@@ -70,8 +20,7 @@ fn gen_url(cdn_type: CdnType, type_id: &String, hash: &String, image_type: Image
 
     result
 }
-
-pub struct Utils {}
+pub struct Utils;
 impl Utils {
     pub fn gen_url(
         cdn_type: CdnType,
@@ -96,7 +45,7 @@ impl Utils {
     /// A `String` containing the full URL to the user's avatar on Discord's CDN.
     ///
     pub fn get_avatar(type_id: &String, hash: &String) -> String {
-        gen_url(
+        Self::gen_url(
             CdnType::UserAvatar,
             type_id,
             hash,
@@ -122,16 +71,14 @@ impl Utils {
     /// A `String` containing the full URL to the user/guild banner on Discord's CDN.
     ///
     pub fn get_banner(banner_type: BannerType, type_id: &String, hash: &String) -> String {
-        format!(
-            "{}/{}/{}/{}.{}",
-            DISCORD_CDN_BASE,
-            CdnType::Banner(banner_type).as_str(),
+        Self::gen_url(
+            CdnType::Banner(banner_type),
             type_id,
             hash,
             hash.starts_with("a_")
-                .then_some(ImageType::Gif.as_str())
-                .or(Some(ImageType::Png.as_str()))
-                .unwrap()
+                .then_some(ImageType::Gif)
+                .or(ImageType::Png.into())
+                .unwrap(),
         )
     }
 
